11d54e889f34e1bb7b0c87906e467c364693f8fa
Sachin22 <sachintiwari.751858@gmail.com>
2025-01-20 00:42:20 +0530

    add address and card section in home also add cool backgraound

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../style/card.css';
+import card1 from '../images/card/card (1).svg';
+import card2 from '../images/card/card (2).svg';
+import card3 from '../images/card/card (3).svg';
+export default function Card() {
+  return (
+    <div className="card-section">
+      <div className="card">
+        <img src={card1} alt="Card 1" />
+        <h2>Training & Education: Empowering You for the Future</h2>
+        <p>Full Stack Web Development: Master the art of dynamic web app creation. Gain proficiency in front-end and back-end technologies to become a sought-after full stack developer.</p>
+        <button>Learn More</button>
+      </div>
+
+      <div className="card">
+        <img  src={card2} alt="Card 2" />
+        <h2>Web Development: Crafting Engaging and User-Centric Websites
+        </h2>
+        <p>UI/UX Websites: Our expert team of designers and developers creates visually stunning and user-friendly websites that leave a lasting impression. We prioritize user experience and ensure seamless navigation, captivating visuals, and intuitive interfaces.</p>
+        <button>Learn More</button>
+      </div>
+
+      <div className="card">
+        <img  src={card3} alt="Card 3" />
+        <h2>IT Staffing: Unlocking Top Talent for Your Organization</h2>
+        <p>Finding the right talent is crucial for the success of any organization. With our IT staffing services, we help you identify and recruit skilled professionals who align with your specific requirements. Our extensive network and rigorous screening process ensure you have access to the best-fit candidates.</p>
+        <button>Larn More</button>
+      </div>
+    </div>
+  )
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,31 @@ import CountUpSection from './CountUpSection';
 import address from '../images/DESTINATION-1024x1001.png';
 import '../style/address.css';
 import { FaArrowRightLong } from "react-icons/fa6";
+import Card from './Card';
+
 export default function Home() {
     return (
         <>
-            <section className="hero">
-                <div className="hero-content">
-                    <h6>
-                        Welcome to Destination Technologies</h6>
-                    <p>One stop Solution for Consulting, Recruitment & Training
-                    </p>
-                    <p style={{ fontSize: '1rem' }}>With Destination Technologies as your trusted partner, you can unlock endless possibilities in your professional journey. Our aim is to empower individuals and organizations alike by bridging the gap between talent and opportunity. Experience our unparalleled services and witness the transformation firsthand.</p>
-                    <button onClick={() => alert('Explore Now!')}>Explore Now</button>
-                </div>
-                <div className="hero-image">
-                    <div className="image-slider"></div>
+            <section>
+                <div className='hero'>
+                    <div className="hero-content">
+                        <h6>Welcome to Destination Technologies</h6>
+                        <p>One stop Solution for Consulting, Recruitment & Training</p>
+                        <p style={{ fontSize: '1rem' }}>
+                            With Destination Technologies as your trusted partner, you can unlock endless possibilities in your professional journey. Our aim is to empower individuals and organizations alike by bridging the gap between talent and opportunity. Experience our unparalleled services and witness the transformation firsthand.
+                        </p>
+                        <button onClick={() => alert('Explore Now!')}>Explore Now</button>
+                    </div>
+
+                    <div className="hero-image">
+                        <div className="image-slider"></div>
+                    </div>
                 </div>
             </section>
-            <Brand />
-            <CountUpSection />
+            <div className='brand2'>
+                <Brand />
+                <CountUpSection />
+            </div>
             {/* address */}
             <section
                 className="address-top" >
@@ -43,19 +50,14 @@ export default function Home() {
                     <img src={address} />
                 </div>
             </section>
-            {/* <svg viewBox="0 0 1440 80" className="wave-svg">
-                <path fill="#00b4d8" d="M0,60 C360,120 720,0 1080,60 C1440,120 1800,0 2160,60 L2160,80 L0,80 Z"></path>
-            </svg> */}
-            <svg viewBox="0 0 1440 80" className="wave-svg">
-                <path fill="#00b4d8" d="
-      M0,40 
-      C120,70 240,10 360,40 
-      C480,70 600,10 720,40 
-      C840,70 960,10 1080,40 
-      C1200,70 1320,10 1440,40 
-      L1440,80 L0,80 Z">
-                </path>
-            </svg>
+            {/* Cards */}
+            <section id='card'>
+                <hr />
+                <h2>Our Services</h2>
+                <hr />
+                <h1>Comprehensive Services to Fuel Your Success</h1>
+                <Card />
+            </section>
         </>
     )
 }
